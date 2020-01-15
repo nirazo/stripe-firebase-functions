26a6fee5fa97614ae1c8864bf5820a23acc74099
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2020-01-15 14:53:53 +0900

    支払い処理実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,35 +8,48 @@ const stripe = require('stripe')(functions.config().stripe.token)
 
 // stripeのcustomerを作成してcustomIdを返す
 exports.createStripeCustomer = functions.https.onCall(async (data, context) => {
-    const email = data.emaill
+    const email = data.email
     const customer = await stripe.customers.create({email: email});
     const customerId = customer.id
     return { customerId: customerId }
 });
 
 // Stripeのワンタイムトークン発行
-exports.createStripeEphemeralKeys = functions.https.onCall((data, context) => {
+exports.createStripeEphemeralKeys = functions.https.onCall(async (data, context) => {
     const customerId = data.customerId;
     const stripe_version = data.stripe_version;
-    return stripe.ephemeralKeys
+    return await stripe.ephemeralKeys
     .create({
-        customer: customerId,
+        customer: customerId 
+    }, {
         stripe_version: stripe_version
     })
 });
 
-// Stripeで決済
-exports.createStripeCharge = functions.https.onCall((data, context) => {
+// Stripeで決済(これだとエラーが出るのでdeprecated)
+exports.createStripeCharge = functions.https.onCall(async (data, context) => {
     const customer = data.customerId;
     const source = data.sourceId;
     const amount = data.amount;
 
-    return stripe.charges.create({
+    return await stripe.charges.create({
         customer: customer,
         source: source,
         amount: amount,
         currency: 'jpy',
     })
+});
+
+exports.createStripePaymentIntents = functions.https.onCall(async (data, context) => {
+    const customer = data.customerId;
+    const amount = data.amount;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        customer: customer,
+        amount: amount,
+        currency: 'jpy',
+    })
+   return {client_secret: paymentIntent.client_secret}
 });
 
 
